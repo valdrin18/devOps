f2583d871a33a03c1023a7ae7db8ef4a67dcfc5c
valdrin18 <72058592+valdrin18@users.noreply.github.com>
2022-12-01 17:26:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
  
-echo -n "enter the num1 ==> "
-read num1
-echo -n "enter the num2 ==> "
-read num2
-sum=$(( $num1 + $num2 ))
-echo "the sum of $num1 and $num2 is ==> $sum"
+read a
+read b
+
+echo “$a+$b=$[a+b]”
